a3fae04e1ad69f80a4c3c8b1308429149069f878
Mugi <noroyt1207@gmail.com>
2025-10-23 11:12:52 +0800

    Add files via upload

---
+++ GameScreen.js
@@ -0,0 +1,99 @@
+import { Alert, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import Title from "../component/Title";
+import color from "../constant/color";
+import PrimaryButton from "../component/PrimaryButton";
+const GameScreen = ({ enteredNumber, checkGameOver }) => {
+  const Intnumber = parseInt(enteredNumber);
+  // enteredNumber 55 taasan 7s baga 7-99 92 7-999
+  const [ami, setAmi] = useState(1);
+  const randomNumber = (max, min) => {
+    let number = Math.floor(Math.random() * (max - min) + min);
+    return number;
+  };
+  const [guesNumber, setGuesNumber] = useState(randomNumber(99, 1));
+  const taahLOW = () => {
+    if (Intnumber > guesNumber) {
+      Alert.alert("Анхаар", "Зөв байна", [{ text: "Cancel" }]);
+      setGuesNumber(randomNumber(Intnumber, guesNumber));
+    } else if (Intnumber == guesNumber) {
+      checkGameOver();
+    } else {
+      if (ami > 0) {
+        setAmi(ami - 1);
+        Alert.alert("Анхаар", "Буруу байна", [{ text: "Cancel" }]);
+      } else checkGameOver();
+    }
+  };
+  const taahHIGH = () => {
+    if (Intnumber < guesNumber) {
+      Alert.alert("Анхаар", "Зөв байна", [{ text: "Cancel" }]);
+      setGuesNumber(randomNumber(guesNumber, Intnumber));
+    } else if (Intnumber == guesNumber) {
+      checkGameOver();
+    } else {
+      if (ami > 0) {
+        setAmi(ami - 1);
+        Alert.alert("Анхаар", "Буруу байна", [{ text: "Cancel" }]);
+      } else if (ami === 0) {
+        checkGameOver();
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Title>GameScreen</Title>
+      <View style={styles.numberContainer}>
+        <Text style={styles.numberStyle}>{guesNumber}</Text>
+        {/* <Text>{Intnumber}</Text> */}
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <Text style={styles.textStyle}>Live:</Text>
+        <Text style={styles.textStyle}>{ami}</Text>
+      </View>
+      <View>
+        {" "}
+        <Text style={styles.textStyle}>HIGH OR LOW ?</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <PrimaryButton onPress={taahLOW}>LOW</PrimaryButton>
+        <PrimaryButton onPress={taahHIGH}>HIGH</PrimaryButton>
+      </View>
+    </View>
+  );
+};
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "start",
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 20,
+    gap: 10,
+  },
+  numberContainer: {
+    padding: 2,
+    borderWidth: 2,
+    borderColor: color.yellow500,
+    width: 100,
+  },
+  numberStyle: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    color: color.yellow500,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  textStyle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: color.yellow500,
+  },
+});
